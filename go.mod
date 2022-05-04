408a793dd89610a733785064fd932b784a983213
Sakari Ekqvist <ekq.sakke@gmail.com>
2022-05-04 21:56:50 +0300

    Setting active window position

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module window-manager
 
 go 1.18
 
-require golang.design/x/hotkey v0.3.0
+require (
+	github.com/iamacarpet/go-win64api v0.0.0-20220420140031-e6e5e4bf90e4
+	golang.design/x/hotkey v0.3.0
+)
+
+require (
+	github.com/JamesHovious/w32 v1.2.0
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/cabbie v1.0.2 // indirect
+	github.com/google/glazier v0.0.0-20211029225403-9f766cca891d // indirect
+	github.com/scjalliance/comshim v0.0.0-20190308082608-cf06d2532c4e // indirect
+	golang.org/x/sys v0.0.0-20211107104306-e0b2ad06fe42 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"sync"
+	"syscall"
+	"unsafe"
 
 	"golang.design/x/hotkey"
+	"golang.org/x/sys/windows"
+)
+
+var (
+	w32                     = windows.NewLazyDLL("user32.dll")
+	procGetWindowText       = w32.NewProc("GetWindowTextW")
+	procGetWindowTextLength = w32.NewProc("GetWindowTextLengthW")
+	procGetForegroundWindow = w32.NewProc("GetForegroundWindow")
+	procSetWindowPos        = w32.NewProc("SetWindowPos")
+)
+
+type (
+	HANDLE uintptr
+	HWND   HANDLE
 )
 
 func check(err error) {
@@ -13,36 +30,73 @@ func check(err error) {
 }
 
 func main() {
+
 	wg := sync.WaitGroup{}
 	wg.Add(2)
-	mods := []hotkey.Modifier{ hotkey.ModShift, hotkey.ModCtrl }
+	mods := []hotkey.Modifier{hotkey.ModShift, hotkey.ModCtrl}
 	go func() {
 		defer wg.Done()
-
-		check(listenHotkey(func(){
+		check(listenHotkey(func() {
+			test()
 			println("I")
 		}, hotkey.KeyI, mods))
 	}()
 	go func() {
 		defer wg.Done()
-
-		check(listenHotkey(func(){
+		check(listenHotkey(func() {
 			println("K")
 		}, hotkey.KeyK, mods))
 	}()
 	wg.Wait()
 }
 
-func listenHotkey(onKeyDown func(), key hotkey.Key, mods []hotkey.Modifier,) (err error) {
+func listenHotkey(onKeyDown func(), key hotkey.Key, mods []hotkey.Modifier) (err error) {
 	ms := []hotkey.Modifier{}
 	ms = append(ms, mods...)
 	hk := hotkey.New(ms, key)
 
 	check(hk.Register())
-	
-	// Blocks until the hokey is triggered.
+
 	for {
+		// Blocks until the hotkey is triggered.
 		<-hk.Keydown()
 		onKeyDown()
 	}
+}
+
+func test() {
+	if hwnd := getWindow(); hwnd != 0 {
+		text := GetWindowText(HWND(hwnd))
+		fmt.Println("window :", text, "# hwnd:", hwnd)
+
+		setWindowPosition(hwnd, 1000, 500, 1000, 300)
+	}
+}
+
+func GetWindowTextLength(hwnd HWND) int {
+	ret, _, _ := procGetWindowTextLength.Call(
+		uintptr(hwnd))
+
+	return int(ret)
+}
+
+func GetWindowText(hwnd HWND) string {
+	textLen := GetWindowTextLength(hwnd) + 1
+
+	buf := make([]uint16, textLen)
+	procGetWindowText.Call(
+		uintptr(hwnd),
+		uintptr(unsafe.Pointer(&buf[0])),
+		uintptr(textLen))
+
+	return syscall.UTF16ToString(buf)
+}
+
+func getWindow() uintptr {
+	hwnd, _, _ := procGetForegroundWindow.Call()
+	return hwnd
+}
+
+func setWindowPosition(hwnd uintptr, x, y, width, height int) {
+	procSetWindowPos.Call(hwnd, 0, uintptr(x), uintptr(y), uintptr(width), uintptr(height), 0)
 }
